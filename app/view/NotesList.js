283f0072dff56941b9d67b108deea1ca09506d7b
Joakim Waltersson <joakim.waltersson@gmail.com>
2012-07-09 16:39:52 +0200

    Part 4 - Implemented note list grouping

--- app/view/NotesList.js
+++ app/view/NotesList.js
@@ -5,6 +5,7 @@ Ext.define('NotesApp.view.NotesList', {
         loadingText: 'Loading notes...',
         emptyText: '</pre><div class="notes-list-empty-text">No notes found.</div><pre>',
         onItemDisclosure: true,
+        grouped: true,
         itemTpl: '</pre><div class="list-item-title">{title}</div><div class="list-item-narrative">{narrative}</div><pre>'
     }
 });
